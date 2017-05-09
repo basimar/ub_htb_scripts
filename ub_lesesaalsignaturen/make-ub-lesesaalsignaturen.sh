29709a9bcf568c2f877d37ca6edbe7d07496d75b
Basil (lokal) <basil@ub-catmandu.ub.unibas.ch>
2017-05-09 17:17:08 +0200

    Added ub_lesesaalsignaturen.

---
+++ ub_lesesaalsignaturen/make-ub-lesesaalsignaturen.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo 'Systematik UB Lesesaal'
+
+inputfile='klassifikation-lesesaalub.txt'
+targetdir='/export/www/htdocs/ibb/api/ublesesaal'
+
+backupfile=`perl -MPOSIX -e 'print strftime("tree_items.js.%Y%m%d",localtime)'`
+if [[ -f tree_items.js && ! -f $backupfile ]]; then
+    echo '* backup alte Version'
+    cp tree_items.js $backupfile
+fi
+
+echo '* generiere tree_items.js'
+perl ../htb/generate_treemenu_pro.pl \
+    --in    $inputfile \
+    --out   tree_items.js \
+    --link  "'javascript:sig(\\'%s\\')'" \
+    --top   'Systematik UB Lesesaal'
+
+if [ $? != 0 ]; then
+    echo 'Programm abgebrochen.'
+    exit;
+fi
+
+read -p '* auf ub-webqm.ub.unibas.ch installieren? [j/n] '
+if [ "$REPLY" == "j" ]; then
+    scp tree_items.js webmaster@ub-webqm.ub.unibas.ch:$targetdir
+fi
+
+read -p '* auf www.ub.unibas.ch installieren? [j/n] '
+if [ "$REPLY" == "j" ]; then
+    scp tree_items.js webmaster@www.ub.unibas.ch:$targetdir
+fi
+
+echo '* fertig.'
+echo '* Resultat siehe:'
+echo http://ub-webqm.ub.unibas.ch/ub-hauptbibliothek/recherche/kataloge/lesesaal-systematik/
+echo http://www.ub.unibas.ch/ub-hauptbibliothek/recherche/kataloge/lesesaal-systematik/
+
+
